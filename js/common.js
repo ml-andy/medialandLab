085497ae028962cfa5e999939262496a995b4b1e
AndyWang2015 <andy2472625@gmail.com>
2016-12-16 19:41:37 +0800

    連線

---
+++ js/common.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function(){
+    var socket = io();
+
+    socket.on('ioAlert',function(data){
+        console.log(data.msg);
+    });
+    socket.on('imgGo',function(data){
+        $('.wrapper img').addClass('on');
+        
+    });
+    
+    $(window).load(function(){
+        $('.wrapper .word').html($(window).height());
+        $('.wrapper .word').click(function(){
+            socket.emit('imgGo');
+        });
+    });
+
+})//ready end
+function getUrlVars(){
+    var vars=[],hash;var hashes=window.location.href.slice(window.location.href.indexOf('?')+1).split('&');
+    for(var i=0;i<hashes.length;i++){hash=hashes[i].split('=');vars.push(hash[0]);vars[hash[0]]=hash[1]}
+    return vars
+}
---
+++ socket.js
@@ -0,0 +1,49 @@
+//socket
+
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var users=[],teams=[];
+
+
+
+app.use(express.static(__dirname + '/andy'));
+__dirname = __dirname +'';
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/andy/index.html');
+});
+
+io.on('connection', function(socket){
+	users.push(socket);
+	console.log('user:'+socket.id+' connected');
+	searhUser(socket.id).emit('ioAlert',{msg:'v 0.17'});
+	
+	socket.on('imgGo', function(){
+		io.emit('imgGo', { for: 'everyone' });
+	});
+	
+	// socket.on('disconnect', function(){
+	//     console.log('user:'+socket.id+' disconnected');
+	    
+	// });
+	// socket.on('enterRoom', function(data){
+		
+	// });
+	// socket.on('createTeam', function(teamsdata){
+		
+	// });
+	
+	
+
+});
+function searhUser(_id){
+	var _thisuser;
+	for(var j in users){				
+		if(users[j].id == _id) _thisuser = users[j];
+	}
+	return _thisuser;
+}
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
